ebaaed06aecad6cc3a4f39bcf5a825805f995a77
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2025-02-16 11:26:40 +0100

    Initial Commit

---
+++ CounterProject/CounterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterProjectApp.swift
+//  CounterProject
+//
+//  Created by Roman Gorbenko on 16.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CounterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
